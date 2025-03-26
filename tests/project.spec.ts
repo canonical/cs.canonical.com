c6005edd91f37b1c93d5b80272286af6ebddba7a
Muhammad Ali <muhammad.ali@canonical.com>
2025-03-26 14:05:16 +0500

    feat: verify page creation and submission

--- tests/project.spec.ts
+++ tests/project.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { config } from "./config";
+import { startVPN, stopVPN } from "./helpers";
 
 test.describe("Test project actions", () => {
   test.beforeEach(async ({ page }) => {
@@ -49,5 +50,51 @@ test.describe("Test project actions", () => {
     }
     await modal.getByRole("button", { name: /Submit/i }).click();
     expect(page.locator(".l-notification__container .p-notification--negative")).not.toBeVisible();
+  });
+
+  test("create new page", async ({ page }) => {
+    // TODO: remove before merge. Only for testing locally
+    await startVPN();
+    await page.getByRole("button", { name: /Request new page/i }).click();
+    await expect(page.getByRole("heading", { name: /New page/i })).toBeVisible();
+    await page.locator("input[aria-labelledby='url-title']").fill(config.PLAYWRIGHT_TEST_PAGE_URL);
+    await page.locator(".l-new-webpage--location .p-list-tree .p-list-tree__item").first().click();
+    await page.getByPlaceholder("Select an owner").fill(config.PLAYWRIGHT_TEST_USER);
+    await page.getByRole("button", { name: config.PLAYWRIGHT_TEST_USER }).click();
+    await page.getByPlaceholder("Select reviewers").fill(config.PLAYWRIGHT_TEST_USER);
+    await page.getByRole("button", { name: config.PLAYWRIGHT_TEST_USER }).click();
+    const productsDropdown = page.getByRole("combobox", { name: "Select products" });
+    await productsDropdown.click();
+    const products = productsDropdown.locator("xpath=parent::*").locator(".multi-select__dropdown-item");
+    expect(await products.count()).toBeGreaterThan(0);
+    for (let i = 0; i < 3; i++) {
+      await products.nth(i).click();
+    }
+    await productsDropdown.click();
+
+    // TODO: remove before merge. Only for testing locally
+    await stopVPN();
+
+    await page.getByRole("button", { name: /Save and generate copy doc/i }).click();
+
+    await page.waitForTimeout(10000);
+    expect(page.url().endsWith(config.PLAYWRIGHT_TEST_PAGE_URL)).toBeTruthy();
+    await page.getByRole("button", { name: /Submit for publication.*/i }).click();
+    const modal = page.locator(".p-modal").first();
+    await expect(modal).toBeVisible();
+    await modal.locator('input[type="date"]').fill(new Date().toISOString().split("T")[0]);
+    const checkboxes = await page.locator("input[type='checkbox'][required]");
+    if (checkboxes) {
+      const checkboxCount = await checkboxes.count();
+      for (let i = 0; i < checkboxCount; i++) {
+        await checkboxes.nth(i).check();
+      }
+    }
+    await modal.getByRole("button", { name: /Submit/i }).click();
+    expect(page.locator(".l-notification__container .p-notification--negative")).not.toBeVisible();
+  });
+
+  test.afterAll(() => {
+    // Todo: after all the tests are done, we can run the cleanup service that will discard jira tickets created during tests.
   });
 });
