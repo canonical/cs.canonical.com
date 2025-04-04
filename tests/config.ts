7b617e487c90e1953006c7db760c99c6b35493cd
Muhammad Ali <muhammad.ali@canonical.com>
2025-04-04 10:39:11 +0500

    testing workflow with docker instead of dotrun

--- tests/config.ts
+++ tests/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  BASE_URL: `http://0.0.0.0:${process.env.PORT}`,
+  BASE_URL: `http://localhost`,
   PLAYWRIGHT_TEST_USER: "Muhammad Ali", // TODO: replace with a dedicated test user account name from IS
   PLAYWRIGHT_TEST_PAGE_URL: "playwright-test-page",
 };
