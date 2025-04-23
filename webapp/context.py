b73c4ac358c6882589135c1052fcbddab4db700b
Samuel Olwe <sam.olwe@gmail.com>
2025-04-23 19:39:14 +0300

    feat: use filecache lock

--- webapp/context.py
+++ webapp/context.py
@@ -1,9 +1,14 @@
 import os
+from collections.abc import Generator
+from contextlib import contextmanager
 from hashlib import md5
+from time import sleep
 from urllib.parse import unquote, urlparse, urlunparse
 
 from flask import current_app, redirect, request
 from werkzeug.routing import BaseConverter
+
+from webapp.cache import FileCache
 
 
 def versioned_static(filename):
@@ -47,6 +52,38 @@ def clear_trailing_slash():
         new_uri = urlunparse(parsed_url._replace(path=path[:-1]))
 
         return redirect(new_uri)
+
+
+DB_LOCK_NAME = "db_lock"
+
+
+@contextmanager
+def file_cache_lock() -> Generator:
+    """
+    A context manager for acquiring a file-based lock to control access
+    to shared resources.
+
+    This function creates a distributed lock using FileCache to ensure only
+    one process can access a protected resource at a time. If the lock is
+    already acquired by another process, this will poll every 2 seconds until
+    the lock is released.
+
+    Yields:
+        The current lock status from the cache
+
+    Example:
+        with file_cache_lock():
+            . . .
+    """
+    cache = FileCache(current_app)
+    locked = cache.get(DB_LOCK_NAME)
+    while locked:
+        sleep(2)
+    try:
+        cache.set(DB_LOCK_NAME, 1)
+        yield cache.get(DB_LOCK_NAME)
+    finally:
+        cache.set(DB_LOCK_NAME, 0)
 
 
 class RegexConverter(BaseConverter):
--- webapp/models.py
+++ webapp/models.py
@@ -24,6 +24,8 @@ from sqlalchemy.orm import (
 )
 from sqlalchemy.orm.session import Session
 
+from webapp.context import file_cache_lock
+
 with open("data/data.yaml") as file:
     data = yaml.load(file, Loader=yaml.FullLoader)
 
@@ -200,9 +202,8 @@ def init_db(app: Flask):
 
     db.init_app(app)
     # Use lock to prevent multiple concurrent migrations on startup
-    # with shared_memory_lock(), app.app_context():
     # Automatically upgrade to head revision
-    with app.app_context():
+    with app.app_context(), file_cache_lock():
         upgrade()
 
     @app.before_request
