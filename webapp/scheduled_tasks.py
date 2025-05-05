260669fadb62028403a2859ea072d4a20b49e21a
Samuel Olwe <sam.olwe@gmail.com>
2025-05-05 17:59:54 +0300

    feat: use app context in scheduled tasks

--- webapp/scheduled_tasks.py
+++ webapp/scheduled_tasks.py
@@ -21,21 +21,22 @@ def load_site_trees() -> None:
     Load the site trees from the queue.
     """
     app = create_app()
-    app.logger.info("Running scheduled task: load_site_trees")
+    with app.app_context():
+        app.logger.info("Running scheduled task: load_site_trees")
 
-    github = app.config.get("github")
-    if not github:
-        app.logger.error("GitHub configuration not found")
-        return
+        github = app.config.get("github")
+        if not github:
+            app.logger.error("GitHub configuration not found")
+            return
 
-    yaml_path = Path(BASE_DIR) / "data/sites.yaml"
-    with yaml_path.open("r") as f:
-        data = yaml.safe_load(f)
-        for site in data["sites"]:
-            # Enqueue the sites for setup
-            site_repository = SiteRepository(site, app, db=db)
-            # build the tree from GH source without using cache
-            site_repository.get_tree(no_cache=True)
+        yaml_path = Path(BASE_DIR) / "data/sites.yaml"
+        with yaml_path.open("r") as f:
+            data = yaml.safe_load(f)
+            for site in data["sites"]:
+                # Enqueue the sites for setup
+                site_repository = SiteRepository(site, app, db=db)
+                # build the tree from GH source without using cache
+                site_repository.get_tree(no_cache=True)
 
 
 @register_task(delay=UPDATE_STATUS_DELAY)
@@ -47,34 +48,36 @@ def update_jira_statuses() -> None:
         app (Flask): The Flask application instance.
     """
     app = create_app()
-    app.logger.info("Running scheduled task: update_jira_statuses")
-    jira = app.config.get("JIRA")
-    if not jira:
-        app.logger.error("JIRA configuration not found")
-        return
-    jira_tasks = JiraTask.query.all()
-    if jira_tasks:
-        project_ids = []
-        for task in jira_tasks:
-            response = jira.get_issue_statuses(task.jira_id)
-            if task.status != response["fields"]["status"]["name"].upper():
-                task.status = response["fields"]["status"]["name"].upper()
-                # get the project id from the webpage that corresponds to
-                # the Jira task (will be needed to invalidate the cache)
-                webpage = Webpage.query.filter_by(
-                    id=task.webpage_id,
-                ).first()
-                if webpage.project_id not in project_ids:
-                    project_ids.append(webpage.project_id)
-        db.session.commit()
+    with app.app_context():
+        app.logger.info("Running scheduled task: update_jira_statuses")
 
-        # invalidate the cache for all the project trees where Jira tasks
-        # have changed status
-        for project_id in project_ids:
-            project = Project.query.filter_by(id=project_id).first()
-            site_repository = SiteRepository(project.name, app)
-            # clean the cache for a new Jira task to appgear in the tree
-            site_repository.invalidate_cache()
+        jira = app.config.get("JIRA")
+        if not jira:
+            app.logger.error("JIRA configuration not found")
+            return
+        jira_tasks = JiraTask.query.all()
+        if jira_tasks:
+            project_ids = []
+            for task in jira_tasks:
+                response = jira.get_issue_statuses(task.jira_id)
+                if task.status != response["fields"]["status"]["name"].upper():
+                    task.status = response["fields"]["status"]["name"].upper()
+                    # get the project id from the webpage that corresponds to
+                    # the Jira task (will be needed to invalidate the cache)
+                    webpage = Webpage.query.filter_by(
+                        id=task.webpage_id,
+                    ).first()
+                    if webpage.project_id not in project_ids:
+                        project_ids.append(webpage.project_id)
+            db.session.commit()
+
+            # invalidate the cache for all the project trees where Jira tasks
+            # have changed status
+            for project_id in project_ids:
+                project = Project.query.filter_by(id=project_id).first()
+                site_repository = SiteRepository(project.name, app)
+                # clean the cache for a new Jira task to appgear in the tree
+                site_repository.invalidate_cache()
 
 
 def init_scheduled_tasks() -> None:
