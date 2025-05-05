bd29b15206117e2f6904fe72f16a5863ece0cb22
Muhammad Ali <muhammad.ali@canonical.com>
2025-05-05 12:20:22 +0500

    chore: clean up routes

--- static/client/services/routes/index.tsx
+++ static/client/services/routes/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import { Route } from "react-router-dom";
 
-import MainLayout from "@/components/MainLayout";
 import NewWebpage from "@/pages/NewWebpage";
 import Webpage from "@/pages/Webpage";
 import { type IPage } from "@/services/api/types/pages";
@@ -11,23 +10,11 @@ export function generateRoutes(project: string, pages: IPage[]): JSX.Element[] {
   return pages.map((page, index) => (
     <React.Fragment key={`${page.name}-${index}`}>
       <Route
-        element={
-          <MainLayout>
-            <Webpage page={page} project={project} />
-          </MainLayout>
-        }
+        element={<Webpage page={page} project={project} />}
         key={page.name}
-        path={`/app/webpage/${project}${page.name}`}
+        path={`webpage/${project}${page.name}`}
       />
-      <Route
-        element={
-          <MainLayout>
-            <NewWebpage />
-          </MainLayout>
-        }
-        key="new-webpage"
-        path="/app/new-webpage"
-      />
+      <Route element={<NewWebpage />} key="new-webpage" path="new-webpage" />
       {page.children?.length && generateRoutes(project, page.children)}
     </React.Fragment>
   ));
