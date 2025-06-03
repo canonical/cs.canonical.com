1b387a9b2db788ef7753fa9e49955996a04817d5
Muhammad Ali <muhammad.ali@canonical.com>
2025-06-03 12:51:23 +0500

    Disable breadcrumbs for non-webpage url paths and vice versa
    drive-by: modify findPage helper to conditionally return bool or page object

--- static/client/services/tree/pages.ts
+++ static/client/services/tree/pages.ts
@@ -1,16 +1,23 @@
 import type { IPage } from "@/services/api/types/pages";
 
 // recursively find the page in the tree by the given name (URL)
-export function findPage(tree: IPage, pageName: string, prefix: string = ""): boolean {
+export function findPage(
+  tree: IPage,
+  pageName: string,
+  prefix: string = "",
+  returnObject: boolean = false,
+): boolean | IPage {
   const parts = pageName.split("/");
+
   for (let i = 0; i < tree.children.length; i += 1) {
     if (tree.children[i].name === `${prefix}/${parts[1]}`) {
       if (parts.length > 2) {
-        return findPage(tree.children[i], `/${parts.slice(2).join("/")}`, `${prefix}/${parts[1]}`);
+        return findPage(tree.children[i], `/${parts.slice(2).join("/")}`, `${prefix}/${parts[1]}`, returnObject);
       }
-      return true;
+      return returnObject ? tree.children[i] : true;
     }
   }
+
   return false;
 }
 
