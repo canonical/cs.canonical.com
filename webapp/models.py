a188325af38a61385ef9125deb749f7341792fea
Samuel Olwe <sam.olwe@gmail.com>
2025-06-17 15:34:02 +0300

    fix: lint

--- webapp/models.py
+++ webapp/models.py
@@ -3,7 +3,6 @@ from datetime import datetime, timezone
 
 import yaml
 from flask import Flask
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import (
     Column,
