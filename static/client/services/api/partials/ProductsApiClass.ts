6989def7ad94272c89c991efda71136c7c76b0cf
immortalcodes <21112002mj@gmail.com>
2025-09-04 18:31:38 +0530

    client product api services

--- static/client/services/api/partials/ProductsApiClass.ts
+++ static/client/services/api/partials/ProductsApiClass.ts
@@ -1,10 +1,19 @@
 import { BasicApiClass } from "./BasicApiClass";
 
 import { ENDPOINTS, REST_TYPES } from "@/services/api/constants";
-import type { IProductsResponse } from "@/services/api/types/products";
+import type { ICRUDProductResponse, IProductsResponse } from "@/services/api/types/products";
 
 export class ProductsApiClass extends BasicApiClass {
   public getProducts(): Promise<IProductsResponse> {
     return this.callApi<IProductsResponse>(ENDPOINTS.getProducts, REST_TYPES.GET);
+  }
+  public addProduct(name: string): Promise<ICRUDProductResponse> {
+    return this.callApi(ENDPOINTS.crudProduct, REST_TYPES.POST, { name: name });
+  }
+  public editProduct(productId: number, name: string): Promise<ICRUDProductResponse> {
+    return this.callApi(ENDPOINTS.crudProduct + `/${productId}`, REST_TYPES.PUT, { name: name });
+  }
+  public deleteProduct(productId: number): Promise<ICRUDProductResponse> {
+    return this.callApi(ENDPOINTS.crudProduct + `/${productId}`, REST_TYPES.DELETE);
   }
 }
