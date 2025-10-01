f0565d0edb7060bbe61f09f802bc8c08791b0022
Muhammad Ali <its.muhammad.ali.mughal@gmail.com>
2025-10-01 16:48:28 +0500

    test(WD-27918): Add playwright test for creating a new webpage with existing copydoc (#243)

--- tests/config.ts
+++ tests/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   BASE_URL: `http://localhost`,
   PLAYWRIGHT_TEST_PAGE_URL: "playwright-test-page",
+  SAMPLE_COPYDOC_URL: "https://docs.google.com/document/d/1EPA_Ea8ShIvyftAc9oVxZYUIMHfAPFF6S5x6FOvLkwM",
 };
