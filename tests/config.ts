a05e15c003b3d450346e724cb1da405ce45208d1
Muhammad Ali <muhammad.ali@canonical.com>
2025-10-06 19:02:42 +0500

    perf: use playwright docker image

--- tests/config.ts
+++ tests/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  BASE_URL: `http://localhost`,
+  BASE_URL: process.env.BASE_URL || `http://localhost:${process.env.PORT}`,
   PLAYWRIGHT_TEST_PAGE_URL: "playwright-test-page",
   SAMPLE_COPYDOC_URL: "https://docs.google.com/document/d/1EPA_Ea8ShIvyftAc9oVxZYUIMHfAPFF6S5x6FOvLkwM",
 };
