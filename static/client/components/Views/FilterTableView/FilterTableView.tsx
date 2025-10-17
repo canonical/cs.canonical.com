1099a20af29affe9eb89c30bfc4fc0cbafa7179b
Muhammad Ali <muhammad.ali@canonical.com>
2025-10-17 17:51:17 +0500

    chore: hide quick actions from personal views, per UX suggestion

--- static/client/components/Views/FilterTableView/FilterTableView.tsx
+++ static/client/components/Views/FilterTableView/FilterTableView.tsx
@@ -6,6 +6,7 @@ import FilterandSearch from "./FilterandSearch";
 
 import DashboardActions from "@/components/DashboardActions";
 import TableView from "@/components/Views/TableView";
+import { VIEW_OWNED, VIEW_REVIEWED } from "@/config";
 import { useViewsStore } from "@/store/views";
 
 const FilterTableView = (): ReactNode => {
@@ -15,18 +16,27 @@ const FilterTableView = (): ReactNode => {
   const viewTitle = useMemo(() => {
     if (location.pathname === "/app") return "All pages";
     switch (view) {
-      case "owned":
+      case VIEW_OWNED:
         return "Owned by me";
-      case "reviewed":
+      case VIEW_REVIEWED:
         return "Reviewed by me";
       default:
         return "All pages";
     }
   }, [location.pathname, view]);
+
+  const showQuickActions = useMemo(() => {
+    return location.pathname === "/app";
+  }, [location.pathname]);
   return (
     <>
-      <DashboardActions />
-      <hr className="p-rule" />
+      {showQuickActions && (
+        <>
+          <DashboardActions />
+          <hr className="p-rule" />
+        </>
+      )}
+
       <h2>{viewTitle}</h2>
       <FilterandSearch />
       <TableView />
