f4a6a8323d2affe4f2186341114276591eb43c1c
Akbar Abdrakhmanov <kz.akbar@gmail.com>
2025-10-21 12:27:43 +0500

    fix spacing in the left sidebar

--- static/client/components/Navigation/NavigationBanner/NavigationBanner.tsx
+++ static/client/components/Navigation/NavigationBanner/NavigationBanner.tsx
@@ -10,7 +10,7 @@ interface NavigationBannerProps {
 
 const NavigationBanner = ({ children }: NavigationBannerProps): ReactNode => {
   const location = useLocation();
-  const homepageLink = { url: "/", label: "Homepage" };
+  const homepageLink = { url: "/app", label: "Homepage" };
   return (
     <>
       <Link
