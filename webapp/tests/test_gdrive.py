140b536edeb979fce63cb9338f39661580825ea8
Samuel Olwe <samuel.olwe@canonical.com>
2026-01-29 15:50:25 +0300

    test: use conftest.py

--- webapp/tests/test_gdrive.py
+++ webapp/tests/test_gdrive.py
@@ -4,7 +4,6 @@ import pytest
 
 from webapp import create_app
 from webapp.gdrive import GoogleDriveClient
-from webapp.tests.fixtures import webpage
 
 class MockService(GoogleDriveClient):
     """
