90e1bb7ffa9b6205d1b0857a0110da4899ae1bb5
Benjamin Oni <onibenjo@gmail.com>
2026-04-17 09:11:05 +0100

    feat(WD-33240): wire FullSiteView to /app/views/table route

--- static/client/pages/Main/Main.tsx
+++ static/client/pages/Main/Main.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import MainLayout from "@/components/MainLayout";
-import FilterTableView from "@/components/Views/FilterTableView";
+import FullSiteView from "@/components/Views/FullSiteView";
 import NewWebpage from "@/pages/NewWebpage";
 import Owned from "@/pages/views/Owned";
 import Requests from "@/pages/views/Requests";
@@ -31,7 +31,7 @@ const Main = (): React.ReactNode => {
             <Route element={<Requests />} index />
             <Route element={<Owned />} path="views/owned" />
             <Route element={<Reviewed />} path="views/reviewed" />
-            <Route element={<FilterTableView />} path="views/table" />
+            <Route element={<FullSiteView />} path="views/table" />
             <Route element={<NewWebpage />} path="new-webpage" />
             {getDynamicRoutes()}
           </Route>
