7cac73bf512c4b1f18779c5a0f417fbec8e382ae
Madhur Jain <21112002mj@gmail.com>
2026-05-01 16:57:57 +0530

    feat: Enhance Releases UI with improved notifications, layout adjustments, and version sorting

--- static/client/services/api/partials/ReleasesApiClass.ts
+++ static/client/services/api/partials/ReleasesApiClass.ts
@@ -1,14 +1,14 @@
 import { BasicApiClass } from "./BasicApiClass";
 
 import { ENDPOINTS, REST_TYPES } from "@/services/api/constants";
-import type { IReleasesData, IReleasesResponse } from "@/services/api/types/releases";
+import type { IReleasesData, IReleasesResponse, IUpdateReleasesResponse } from "@/services/api/types/releases";
 
 export class ReleasesApiClass extends BasicApiClass {
   public getReleases(): Promise<{ data: IReleasesResponse }> {
     return this.callApi<{ data: IReleasesResponse }>(ENDPOINTS.getReleases, REST_TYPES.GET);
   }
 
-  public updateReleases(data: IReleasesData, commitMessage: string): Promise<void> {
-    return this.callApi(ENDPOINTS.updateReleases, REST_TYPES.POST, { releases: data, commit_message: commitMessage });
+  public updateReleases(data: IReleasesData, commitMessage: string): Promise<{ data: IUpdateReleasesResponse }> {
+    return this.callApi<{ data: IUpdateReleasesResponse }>(ENDPOINTS.updateReleases, REST_TYPES.POST, { releases: data, commit_message: commitMessage });
   }
 }
