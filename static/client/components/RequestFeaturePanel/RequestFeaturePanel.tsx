52b352249051799742f08e0c8889f36701954d67
Madhur Jain <21112002mj@gmail.com>
2026-05-04 15:48:49 +0530

    refactor: update error handling

--- static/client/components/RequestFeaturePanel/RequestFeaturePanel.tsx
+++ static/client/components/RequestFeaturePanel/RequestFeaturePanel.tsx
@@ -9,8 +9,6 @@ import {
   Textarea,
   useToastNotification,
 } from "@canonical/react-components";
-import type { AxiosError } from "axios";
-
 import config from "@/config";
 import type { IBasicApiError } from "@/services/api/partials/BasicApiClass";
 import { JiraServices } from "@/services/api/services/jira";
@@ -38,9 +36,9 @@ const RequestFeaturePanel = () => {
   );
 
   const onSubmitError = useCallback(
-    (error: AxiosError<IBasicApiError>) => {
-      if (error?.response?.data) {
-        notify.failure(error.response.data?.error, null, <p>{error.response.data?.description}</p>);
+    (error: IBasicApiError) => {
+      if (error?.error) {
+        notify.failure(error.error, null, <p>{error.description}</p>);
       }
     },
     [notify],
